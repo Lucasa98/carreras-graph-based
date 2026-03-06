ff3c2dac6982f7bb9f20d200c3c255364fcc926f
lucasa98 <lusaurin@gmail.com>
2026-03-06 17:57:29 -0300

    init

---
+++ src/test/java/com/lucas/carreras/CarrerasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucas.carreras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrerasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
